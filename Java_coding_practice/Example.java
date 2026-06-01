be0c1526338685d99e736cd8d8d2d2fd74416dc1
MADHANAGOPAL-NS <madhanns2005@gmail.com>
2026-06-01 13:07:07 +0530

    today studied concepts as code

---
+++ Java_coding_practice/Example.java
@@ -0,0 +1,6 @@
+/* This is a simple java program with file name Example */
+class Example{
+    public static void main(String args[]){
+        System.out.println("This is a simple java program");
+    }
+}
