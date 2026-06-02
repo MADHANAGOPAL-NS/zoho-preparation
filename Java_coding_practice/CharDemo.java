80cd9f2469d67bd9910e68a9aadaf5f9c149553a
MADHANAGOPAL-NS <madhanns2005@gmail.com>
2026-06-02 18:10:27 +0530

    second practice session

---
+++ Java_coding_practice/CharDemo.java
@@ -0,0 +1,12 @@
+public class CharDemo {
+ public static void main(String args[]){
+    char a = 'x';
+    
+
+    System.out.println("The present character is: " + a);
+
+    char ans = (char) (a + 1);
+
+    System.out.print("The present character is: " + ans);
+ }   
+}
