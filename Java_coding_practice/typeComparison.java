64ad0cb4d7c83c17687fa6a8ca206aff2f7b273b
MADHANAGOPAL-NS <madhanns2005@gmail.com>
2026-06-08 14:10:36 +0530

    studied operators in java

---
+++ Java_coding_practice/typeComparison.java
@@ -0,0 +1,9 @@
+class Madhan{}
+
+public class typeComparison {
+    public static void main(String args[]){
+        Madhan m = new Madhan();
+
+        System.out.print(m instanceof Madhan);
+    }
+}
